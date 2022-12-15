f5ff582c4817cc71e926b15d76afd5c78cc94617
BlockJayn <blockjayn@gmail.com>
2022-12-15 13:31:43 +0800

    Main

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,14 +18,9 @@ export default function Home() {
         </Head>
 
         <main className={styles.main}>
-          <h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
+          <h1 className={styles.title}>Welcome to X-Protocol</h1>
 
-          <p className={styles.description}>
-            Get started by editing{" "}
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
+          <p className={styles.description}>Get started now!</p>
 
           <div className={styles.grid}>
             <a href="https://nextjs.org/docs" className={styles.card}>
@@ -33,10 +28,7 @@ export default function Home() {
               <p>Find in-depth information about Next.js features and API.</p>
             </a>
 
-            <a href="https://nextjs.org/learn" className={styles.card}>
-              <h2>Learn &rarr;</h2>
-              <p>Learn about Next.js in an interactive course with quizzes!</p>
-            </a>
+
 
             <a
               href="https://github.com/vercel/next.js/tree/canary/examples"
@@ -46,17 +38,7 @@ export default function Home() {
               <p>Discover and deploy boilerplate example Next.js projects.</p>
             </a>
 
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.card}
-            >
-              <h2>Deploy &rarr;</h2>
-              <p>
-                Instantly deploy your Next.js site to a public URL with Vercel.
-              </p>
-            </a>
+
           </div>
         </main>
 
